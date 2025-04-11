46af257db7ec6175ad678929cdb23e6a9bb53c77
joseonyeong <whtjsdud54@naver.com>
2025-04-11 11:48:27 +0900

    design: modify update fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+read -p "커밋 메시지 입력: " user_message
+
+current_time=$(TZ=Asia/Seoul date "+%Y-%m-%d %H:%M:%S")
+
+commit_message="$user_message | $current_time (KST)"
+
 git add .
-git commit -m "."
+git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
